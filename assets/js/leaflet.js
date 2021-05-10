8f7ca05077470e01aa29789894335ed42a063b88
ADF Muhendislik <7839081-ADFMuhendislik@users.noreply.gitlab.com>
2021-05-11 02:50:00 +0300

    map panto add

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -1,3 +1,24 @@
 var map = L.map("mapid").setView([38.75, 35.414], 15.5);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+var x = 0;
+var y = 0;
+
+document.addEventListener('keydown', (event) => {
+
+    var name = event.key;
+    var code = event.code;
+
+    if (name == 1) {
+        y = y - 0.1;
+        console.log(name)
+        map.panTo(new L.LatLng(38.75 + x, 35.414 + y));
+    }
+    if (name == 2) {
+        x = x + 0.1;
+        console.log(name)
+        map.panTo(new L.LatLng(38.75 + x, 35.414 + y));
+    }
+    console.log(`x : ${x} --- y: ${y}`)
+}, false);
